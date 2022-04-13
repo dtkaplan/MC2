e14f697fde065d9c2591bc73f8c14d61bacbe3d9
Danny Kaplan <kaplan@macalester.edu>
2022-04-13 10:08:49 -0500

    Replaced Figure \@ref(fig:) style cross-references

--- to_quarto.R
+++ to_quarto.R
@@ -1,1 +1,8 @@
-pattern <- "Figure \\@ref\(fig:([^\)]*)\)"
+pattern <- "Figure \\\\@ref\\(fig:([^\\)]*)\\)"
+
+example <- "In Figure \\@ref(fig:ramp-decomposition) this grid"
+
+gsub(pattern, "\\@\\1", example)
+
+# Figure \\@ref\(fig:([^\)]*)\)
+# @fig-\1
